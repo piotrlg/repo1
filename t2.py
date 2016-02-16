63d3d3aa978d0c1cc7bff9199993b2e53a353b1d
piotrlg <piotrlg@users.noreply.github.com>
2016-02-16 20:58:39 +0100

    Jedziem z comitem z pycharma teraz

--- t2.py
+++ t2.py
@@ -1,1 +1,25 @@
 __author__ = 'piotrlg'
+
+
+class a_class(object):
+    def __init__(self, a1):
+        self.atr1 = a1
+        pass
+
+    def meth_1(self):
+        pass
+
+    def meth_2(self):
+        pass
+
+
+def main():
+    a = a_class('jeden')
+    b = a_class(33)
+
+    print type(a.atr1), type(b.atr1)
+
+
+if __name__ == '__main__':
+    main()
+
